dd89831fbda0123d52447c37df75f2f389e28f58
Jailson de Souza <jailson.souza@compasso.com.br>
2023-07-09 16:32:12 -0300

    change progress color

--- src/app/components/Progress/style.tsx
+++ src/app/components/Progress/style.tsx
@@ -6,7 +6,7 @@ interface IProgressStyleProps {
 
 export const Container = styled.div<IProgressStyleProps>`
    width: ${({ percent }) => String(percent) + '%'};
-   background: #6c3;
+   background: #d80075;
    height: 2px;
    transition: all 0.2s ease;
 `
